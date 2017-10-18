cbeeaf52c38d703198cfbc3d2ba5e3b2bab7c50b
Darryl Daniel <Darryl.Daniel@derivco.co.za>
2017-10-18 07:31:12 +0200

    fix: set the port back to 80 for the device

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ app.use(ledRouter);
 app.use(express.static('public'));
 
 //start a server on port 80 and log its start to our console
-const server = app.listen(3000, function () {
+const server = app.listen(80, function () {
 
   var port = server.address().port;
   console.log("Hey… I’m a node.js server running in a container and listening on port: ", port);
